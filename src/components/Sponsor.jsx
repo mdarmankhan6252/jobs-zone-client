979f74e1e2a740b62127dd19372506842f3cf528
MukterHossain <mukter3075@gmail.com>
2024-12-25 15:12:34 +0600

    set img and link on Spnsor component

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -1,12 +1,48 @@
-
-
 const Sponsor = () => {
-    return (
+  return (
+    <>
+      <div className="bg-white my-24">
         <div>
-            <h1>Many Country</h1>
-            <span className="flex h-full w-full items-center justify-center rounded-full bg-gray-50 font-medium text-primary-700">CO</span>
+          <h1 className="text-2xl text-center text-[#565F6E] ">The world's best companies are hiring on Himalayas</h1>
         </div>
-    );
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-center items-center gap-8 mt-12">
+            <div className="">
+                <a target="_blank" className=" " href="https://himalayas.app/companies/corellium"> <img src="https://i.ibb.co.com/pbQbNFW/corellium.png" alt="logo" /></a> 
+            </div>
+            <div className="">
+                <a target="_blank" className="" href="https://himalayas.app/companies/veed-io"> <img src="https://i.ibb.co.com/93Y42BW/Veedio.png" alt="logo" /></a> 
+            </div>
+            <div className="">
+                <a target="_blank" className="" href="https://himalayas.app/companies/strapi"> <img src="https://i.ibb.co.com/VWsbQ3v/Strapi.png" alt="logo" /></a> 
+            </div>
+            <div className="">
+                <a target="_blank" className="" href="https://himalayas.app/companies/blinkist"> <img src="https://i.ibb.co.com/G9JZ3rx/blinkist.png" alt="logo" /></a> 
+            </div>
+            <div className="">
+                <a target="_blank" className="" href="https://himalayas.app/companies/hugging-face"> <img src="https://i.ibb.co.com/V9YNYTH/hugging-face.png" alt="logo" /></a> 
+            </div>
+            <div className="">
+                <a target="_blank" className="" href="https://himalayas.app/companies/fonoa"> <img src="https://i.ibb.co.com/qBY5Qd1/fonoa.png" alt="logo" /></a> 
+            </div>
+            <div className="">
+                <a target="_blank" className="" href="https://himalayas.app/companies/doximity"> <img src="https://i.ibb.co.com/Thn6hcD/doximity.png" alt="logo" /></a> 
+            </div>
+            <div className="">
+                <a target="_blank" className="" href="https://himalayas.app/companies/wikimedia-foundation"> <img src="https://i.ibb.co.com/Cmv50Sz/wikimedia.png" alt="logo" /></a> 
+            </div>
+            <div className="">
+                <a target="_blank" className="" href="https://himalayas.app/companies/percona"> <img src="https://i.ibb.co.com/qC89Lvc/percona.png" alt="logo" /></a> 
+            </div>
+            <div className="">
+                <a target="_blank" className="" href="https://himalayas.app/companies/pulley"> <img src="https://i.ibb.co.com/qpG656T/pulley.png" alt="logo" /></a> 
+            </div>
+            <div className="text-center">
+                <a target="_blank" className="" href="https://himalayas.app/companies/mycase"> <img src="https://i.ibb.co.com/dcL2psZ/mycase.png" alt="logo" /></a> 
+            </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Sponsor;
