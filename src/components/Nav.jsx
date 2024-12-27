0d8ba8353b5f9a3dbb68090fc2a8d6dfd66cbaeb
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2024-12-27 20:14:29 +0600

    I have changged the nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,82 +1,83 @@
 import { Link, NavLink } from 'react-router-dom';
-import {  CiMenuBurger } from "react-icons/ci";
+import { CiMenuBurger } from "react-icons/ci";
 import logo from '../assets/logo.png'
 import { useEffect, useState } from 'react';
 
 
 
 const Nav = () => {
-   const user = true;
 
-
-   const [scrollPosition, setScrollPosition] = useState(0)
-   const [showNav, setShowNav] = useState(true)
-   const [menu, setMenu] = useState(false)
+  const [scrollPosition, setScrollPosition] = useState(0)
+  const [showNav, setShowNav] = useState(true)
+  const [menu, setMenu] = useState(false)
 
 
 
-   useEffect(() => {
-      const handleScroll = () => {
-         const currentScrollPos = window.scrollY;
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
 
-         if (currentScrollPos < scrollPosition) {
-            setShowNav(true)
-         }
-         else {
-            setShowNav(false)
-         }
-         setScrollPosition(currentScrollPos)
+      if (currentScrollPos < scrollPosition) {
+        setShowNav(true)
       }
-
-      window.addEventListener('scroll', handleScroll);
-
-      return () => {
-         window.removeEventListener('scroll', handleScroll)
+      else {
+        setShowNav(false)
       }
-   }, [scrollPosition])
+      setScrollPosition(currentScrollPos)
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [scrollPosition])
 
 
-   return (
-      <nav className={`fixed top-0 left-0 right-0  shadow-sm z-50 bg-purple-100/80 backdrop-blur-3xl transition-transform duration-500 ease-in-out ${showNav ? 'translate-y-0' : '-translate-y-full'}`}>
-         <div className='flex items-center justify-between py-2 max-w-7xl mx-auto px-3'>
-            <Link to='/'>
-               <img src={logo} alt="" className='w-14' />
-            </Link>
-            <ul className='font-semibold text-gray-600 space-x-6 nav items-center hidden md:flex'>
-               <NavLink to='/'>Home</NavLink>
-               <NavLink to='/about'>About</NavLink>
-               <NavLink to='/jobs'>Jobs</NavLink>
-               {user && <NavLink to='/dashboard/statistics'>Dashboard</NavLink>}
+  return (
+    <nav className={`fixed top-0 left-0 right-0  shadow-sm z-50 bg-gray-100/80 backdrop-blur-3xl transition-transform duration-500 ease-in-out ${showNav ? 'translate-y-0' : '-translate-y-full'}`}>
+      <div className='flex items-center justify-between py-2 max-w-7xl mx-auto px-3'>
+        <Link to='/'>
+          <img src={logo} alt="" className='w-14' />
+        </Link>
+        <ul className='font-semibold text-gray-600 space-x-6 nav items-center hidden md:flex'>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/'>About</NavLink>
+          <NavLink to='/'>Jobs</NavLink>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/'>Blog</NavLink>
+          <NavLink to='/'>Contact Us</NavLink>
 
 
-            </ul>
+        </ul>
 
-            <div className='flex items-center space-x-4'>
-               <div className='flex items-center space-x-6 cursor-pointer'>
-                  
-                  <Link to='/profile'><img src="https://i.ibb.co.com/m4xS62K/pexels-anntarazevich-5234256.jpg" referrerPolicy="no-referrer" className='w-10 h-10 rounded-full object-cover' /></Link>
-                  
-               </div>
-               <Link className='font-semibold border-2 py-1.5 px-4 rounded-md border-purple-600 text-purple-600' to='/login'>Login</Link>
-               <div onClick={() => setMenu(true)} className='text-2xl md:hidden cursor-pointer'>
-                  <CiMenuBurger className='' />
-               </div>
-            </div>
+        <div className='flex items-center space-x-4'>
+          <div className='flex items-center space-x-6 cursor-pointer'>
 
-            {/* side menu */}
+            <Link to='/profile'><img src="https://i.ibb.co.com/m4xS62K/pexels-anntarazevich-5234256.jpg" referrerPolicy="no-referrer" className='w-10 h-10 rounded-full object-cover' /></Link>
 
-            <ul className={`font-semibold text-gray-600 nav  absolute flex flex-col items-start bg-white w-[250px] h-screen top-0 right-0 py-5 pl-10 pr-2 space-y-5 md:hidden duration-500  ${menu ? 'mr-0' : '-mr-72'}`}>
-               <NavLink to='/'>Home</NavLink>
-               <NavLink to='/about'>About</NavLink>
-               <NavLink to='/jobs'>Jobs</NavLink>
-               <NavLink to='/dashboard/statistics'>Dashboard</NavLink>
-               <NavLink to='login'>Login</NavLink>
+          </div>
+          <Link className='font-semibold border-2 py-1.5 px-4 rounded-md border-purple-600 text-purple-600' to='/login'>Login</Link>
+          <div onClick={() => setMenu(true)} className='text-2xl md:hidden cursor-pointer'>
+            <CiMenuBurger className='' />
+          </div>
+        </div>
 
-            </ul>
+        {/* side menu */}
 
-         </div>
-      </nav>
-   );
+        <ul className={`font-semibold text-gray-600 nav  absolute flex flex-col items-start bg-white w-[250px] h-screen top-0 right-0 py-5 pl-10 pr-2 space-y-5 md:hidden duration-500  ${menu ? 'mr-0' : '-mr-72'}`}>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/'>About</NavLink>
+          <NavLink to='/'>Jobs</NavLink>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/'>Blog</NavLink>
+          <NavLink to='/'>Contact Us</NavLink>
+
+        </ul>
+
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
