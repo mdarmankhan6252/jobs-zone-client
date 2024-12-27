9a0e5dfe3aebcb6e4768c457eff212d2ab050fc8
MukterHossain <mukter3075@gmail.com>
2024-12-27 10:54:59 +0600

    Change font size and some css style fixed in FindRomoteJob

--- src/pages/Home/FindRemoteJob.jsx
+++ src/pages/Home/FindRemoteJob.jsx
@@ -5,98 +5,98 @@ import { Link } from "react-router-dom";
 
 const FindRemoteJob = () => {
   return (
-    <>
-      <div className="bg-[#F9F9FB] px-3 py-24">
+    <div className="bg-gray-50">
+      <div className=" max-w-7xl mx-auto px-4 py-16   md:py-24 ">
         <div className="flex items-center space-x-3">
           <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
-          <p className="text-lg">Find a remote job</p><span className="hover:translate-x-5 duration-500 hover:transition-transform"><FaArrowRightLong size={20}></FaArrowRightLong> </span>
+          <p className="text-[16px]">Find a remote job</p><span className="hover:translate-x-5 duration-500 hover:transition-transform"><FaArrowRightLong size={20}></FaArrowRightLong> </span>
         </div>
         <div className="flex justify-between items-center my-12">
             <div >
-                <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold">Explore 40,000+ remote job opportunities</h1>
-                <p className="text-sm md:text-xl text-[#888E9A] mt-2">Personalized filters make it quick and easy to find the jobs you care about.</p>
+                <h1 className="mb-3 md:mb-4 text-2xl md:text-3xl font-semibold">Explore 40,000+ remote job opportunities</h1>
+                <p className="md-8  text-gray-600 md:text-xl text-lg ">Personalized filters make it quick and easy to find the jobs you care about.</p>
             </div>
             <div>
                 <button className="btn hover:bg-[#553C9A] bg-[#6a4ac3] text-white font-semibold py-3 px-6 rounded-lg"> Search Job</button>
             </div>
         </div>
         {/* Card section */}
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 md:gap-8">
             <Link to={'/'}>
-            <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
-                <div className="flex justify-between items-center">
-                    <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
-                    <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
+                    <div className="flex justify-between items-center">
+                        <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
+                        <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                    </div>
+                    <div>
+                        <h2 className="text-lg md:text-xl font-medium mb-2 mt-4 text-gray-900">Remote developer jobs</h2>
+                        <p className="text-[16px] text-gray-600 line-clamp-2 ">Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
+                    </div>
                 </div>
-                <div>
-                    <h2 className="text-[#121926] text-lg md:text-xl font-bold pt-4 pb-2">Remote developer jobs</h2>
-                    <p>Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
-                </div>
-            </div>
             </Link>
             <Link to={'/'}>
-            <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
-                <div className="flex justify-between items-center">
-                    <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
-                    <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
+                    <div className="flex justify-between items-center">
+                        <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
+                        <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                    </div>
+                    <div>
+                        <h2 className="text-lg md:text-xl font-medium mb-2 mt-4 text-gray-900">Remote developer jobs</h2>
+                        <p className="text-[16px] text-gray-600 line-clamp-2 ">Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
+                    </div>
                 </div>
-                <div>
-                    <h2 className="text-[#121926] text-lg md:text-xl font-bold pt-4 pb-2">Remote developer jobs</h2>
-                    <p>Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
-                </div>
-            </div>
             </Link>
             <Link to={'/'}>
-            <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
-                <div className="flex justify-between items-center">
-                    <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
-                    <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
+                    <div className="flex justify-between items-center">
+                        <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
+                        <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                    </div>
+                    <div>
+                        <h2 className="text-lg md:text-xl font-medium mb-2 mt-4 text-gray-900">Remote developer jobs</h2>
+                        <p className="text-[16px] text-gray-600 line-clamp-2 ">Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
+                    </div>
                 </div>
-                <div>
-                    <h2 className="text-[#121926] text-lg md:text-xl font-bold pt-4 pb-2">Remote developer jobs</h2>
-                    <p>Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
-                </div>
-            </div>
             </Link>
             <Link to={'/'}>
-            <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
-                <div className="flex justify-between items-center">
-                    <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
-                    <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
+                    <div className="flex justify-between items-center">
+                        <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
+                        <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                    </div>
+                    <div>
+                        <h2 className="text-lg md:text-xl font-medium mb-2 mt-4 text-gray-900">Remote developer jobs</h2>
+                        <p className="text-[16px] text-gray-600 line-clamp-2 ">Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
+                    </div>
                 </div>
-                <div>
-                    <h2 className="text-[#121926] text-lg md:text-xl font-bold pt-4 pb-2">Remote developer jobs</h2>
-                    <p>Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
-                </div>
-            </div>
             </Link>
             <Link to={'/'}>
-            <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
-                <div className="flex justify-between items-center">
-                    <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
-                    <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
+                    <div className="flex justify-between items-center">
+                        <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
+                        <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                    </div>
+                    <div>
+                        <h2 className="text-lg md:text-xl font-medium mb-2 mt-4 text-gray-900">Remote developer jobs</h2>
+                        <p className="text-[16px] text-gray-600 line-clamp-2 ">Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
+                    </div>
                 </div>
-                <div>
-                    <h2 className="text-[#121926] text-lg md:text-xl font-bold pt-4 pb-2">Remote developer jobs</h2>
-                    <p>Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
-                </div>
-            </div>
             </Link>
             <Link to={'/'}>
-            <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
-                <div className="flex justify-between items-center">
-                    <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
-                    <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                <div className="hover:shadow-xl p-8 border-2 border-gray-300 rounded-xl">
+                    <div className="flex justify-between items-center">
+                        <span className="bg-[#DBEAFE] text-green-600 p-3 rounded-full"><BsBox size={25} ></BsBox></span>
+                        <img src="https://i.ibb.co.com/2yNgPDZ/find-Remote-Job.png" alt="photo" />
+                    </div>
+                    <div>
+                        <h2 className="text-lg md:text-xl font-medium mb-2 mt-4 text-gray-900">Remote developer jobs</h2>
+                        <p className="text-[16px] text-gray-600 line-clamp-2 ">Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
+                    </div>
                 </div>
-                <div>
-                    <h2 className="text-[#121926] text-lg md:text-xl font-bold pt-4 pb-2">Remote developer jobs</h2>
-                    <p>Explore remote jobs at companies like Imprint, Kojo and PermitFlow </p>
-                </div>
-            </div>
             </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
