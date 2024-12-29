f0d9bc6d5f76e979f9954d1fe81ccca56e461d02
MukterHossain <MukterHossain>
2024-12-29 10:08:26 +0600

    set PricingHeader in Pricing page and set route in Route

---
+++ src/pages/Pricing/Pricing.jsx
@@ -0,0 +1,12 @@
+import PricingHeader from "./PricingHeader";
+
+
+const Pricing = () => {
+    return (
+        <div>
+            <PricingHeader></PricingHeader>
+        </div>
+    );
+};
+
+export default Pricing;
