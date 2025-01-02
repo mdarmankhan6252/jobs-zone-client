ed7dabb005cd9ee4eae4fe7eec6a41eaf9e441f8
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2025-01-02 20:19:45 +0600

    I've updated about component

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import DreemJob from "../Home/DreemJob";
+import OurPledge from "../Home/OurPledge";
 import AboutHeader from "./AboutHeader";
 import BenefitsTab from "./BenefitsTab";
 import ClaimProfile from "./ClaimProfile";
@@ -13,7 +15,7 @@ const About = () => {
   return (
     <div>
       <AboutHeader activeTabState={activeTabState}></AboutHeader>
-      <div className="max-w-7xl mx-auto px-4 md:px-8 pt-12 md:pt-16 flex flex-col gap-16 xl:flex-row justify-between">
+      <div className="max-w-7xl mx-auto px-4 md:px-8 py-12 md:py-16 flex flex-col gap-16 xl:flex-row justify-between">
         <div className="flex-shrink-0 xl:w-[48rem]">
           {activeTab === "Overview" && <Overview activeTabState={activeTabState}></Overview>}
           {activeTab === "Jobs" && <JobsTab></JobsTab>}
@@ -23,6 +25,8 @@ const About = () => {
         </div>
         <ClaimProfile></ClaimProfile>
       </div>
+      <DreemJob></DreemJob>
+      <OurPledge></OurPledge>
     </div>
   );
 };
