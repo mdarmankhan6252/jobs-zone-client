291f545070ddfbd338a7dd8b2743ed99f8efef7f
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2025-01-02 18:50:50 +0600

    I have added a route in the route page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/Home/Home";
 import Root from "../layouts/Root";
 import Pricing from "../pages/Pricing/Pricing";
 import Blogs from "../pages/Blogs/Blogs";
+import Jobs from "../pages/Jobs/Jobs";
 
 export const router = createBrowserRouter([
    {
@@ -21,6 +22,10 @@ export const router = createBrowserRouter([
             path: '/blogs',
             element: <Blogs></Blogs>
          },
+         {
+            path: '/jobs',
+            element: <Jobs />
+         }
       ]
    },
 ]);
