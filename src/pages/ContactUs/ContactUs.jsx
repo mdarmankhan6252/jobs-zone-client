9f6509ab3b5f2021802c7a406e6142d904654764
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2025-01-03 20:06:19 +0600

    moved map section to Map component in contact us page

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,27 +1,13 @@
-import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import ContactForm from "./ContactForm";
 import ContactInfo from "./ContactInfo";
+import Map from "./Map";
 
 const ContactUs = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 md:px-8 py-16 md:py-24">
       <ContactForm />
       <ContactInfo />
-
-      <div className="mt-12">
-        <MapContainer center={[24.88938953803653, 91.89110781554236]} zoom={15} className="rounded-3xl shadow" style={{ height: "25rem", width: "100%" }}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <Marker position={[24.88938953803653, 91.89110781554236]}>
-            <Popup>
-              Our Office <br /> Street 12, Block A.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </div>
+      <Map />
     </div>
   );
 };
