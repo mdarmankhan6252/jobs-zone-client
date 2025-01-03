16005bc059ab57b0346b687a8eec6b3a409e6db2
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2025-01-03 19:21:15 +0600

    added map in contact page

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,3 +1,6 @@
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
 const ContactUs = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 md:px-8 py-16 md:py-24">
@@ -34,6 +37,20 @@ const ContactUs = () => {
             <button className="w-full py-2 bg-[#6b46c1] transition-all hover:bg-[#553c9a] text-white rounded">Send</button>
           </form>
         </div>
+      </div>
+
+      <div className="mt-12">
+        <MapContainer center={[24.88938953803653, 91.89110781554236]} zoom={15} className="rounded-3xl shadow" style={{ height: "25rem", width: "100%" }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[24.88938953803653, 91.89110781554236]}>
+            <Popup>
+              Our Office <br /> Street 12, Block A.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </div>
   );
