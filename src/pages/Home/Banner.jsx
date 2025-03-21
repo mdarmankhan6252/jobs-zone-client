992ae4e3b5c26408194ce75d9a32b4191c234413
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2025-03-22 00:42:07 +0600

    I have commited

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -6,23 +6,23 @@ const Banner = () => {
   return (
     <section className="flex items-center md:flex-row flex-col justify-between max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-8 sm:my-12">
       <div className="w-full lg:max-w-3xl pb-8 py-20 space-y-6">
-        <h1 style={{lineHeight : '3.5rem'}} className="font-semibold text-4xl sm:text-5xl">
+        <h1 style={{ lineHeight: '3.5rem' }} className="font-semibold text-4xl sm:text-5xl">
           Top Remote Job Board with Powerful AI Job Search Tools
         </h1>
         <p className="md:text-lg text-gray-900/90 font-medium">
           Find remote jobs, research top remote companies, and track your job applications—all for free! Unlock unlimited access to our AI-powered resume builder, cover letter generator, and mock interview practice for just $9/month.
         </p>
 
-        <div className="flex flex-col gap-3 sm:flex-row sm:w-max">
+        <div className="flex flex-col sm:flex-row items-center gap-4">
           <Link
             to="/jobs"
-            className="w-full sm:w-auto font-medium rounded-lg transition-colors focus:ring-4 focus:outline-hidden disabled:text-gray-300 disabled:border-gray-100 border border-gray-200 shadow-2xs hover:bg-gray-50 hover:text-gray-800 bg-white text-gray-700 focus:ring-gray-100 text-sm sm:text-base py-2 sm:py-3 px-4 sm:px-5"
+            className="border py-3 px-6 "
           >
             Hire remote workers
           </Link>
           <Link
             to="/jobs"
-            className="w-full sm:w-auto inline-flex gap-x-2 font-medium items-center justify-center rounded-lg transition-colors focus:ring-4 focus:outline-hidden border border-transparent bg-[#6b46c1] text-white hover:bg-[#553c9a] disabled:bg-[#e9d8fd] focus:ring-[#f5ebff] text-sm sm:text-base py-2 sm:py-3 px-4 sm:px-5"
+            className="w-full sm:w-auto flex items-center justify-center gap-2 font-medium rounded-lg bg-[#6b46c1] text-white hover:bg-[#553c9a] focus:ring-4 focus:ring-[#f5ebff] disabled:bg-[#e9d8fd] text-sm sm:text-base py-2 sm:py-3 px-4 sm:px-5"
           >
             <LuSearch />
             Find a job
